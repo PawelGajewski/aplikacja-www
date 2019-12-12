b308a071867ce8a7379faf208cc57e982b0671b8
PawelGajewski <35578184+PawelGajewski@users.noreply.github.com>
2019-12-12 22:20:26 +0100

    Update serislizers.py

--- warsztat/serislizers.py
+++ warsztat/serislizers.py
@@ -6,11 +6,21 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+        
+        def validate_vin(self, data):
+        if len(data['password']) < 8:
+            raise serializers.ValidationError("User password must be longer than 8 characters!")
+        return data
 
 class CarSerializer(serializers.ModelSerializer):
     class Meta:
         model = Car
         fields = '__all__'
+        
+        def validate_vin(self, data):
+        if len(data['VIN']) != 17:
+            raise serializers.ValidationError("Vin number must be 17 characters length!")
+        return data
 
 class ServiceSerializer(serializers.ModelSerializer):
     class Meta:
